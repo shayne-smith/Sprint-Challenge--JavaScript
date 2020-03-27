967716feca8784469bb6bb1c45bf8d4bdb740fe0
seanmx96 <seanmx96@users.noreply.github.com>
2020-03-27 10:58:13 -0500

    Completed 2 more array challenges.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -80,7 +80,8 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, log the result. */
-const universities = [];
+
+const universities = graduates.map(element => element.university);
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -89,11 +90,17 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map(element => `${element.first_name} ${element.email}`);
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+
+for (let i = 0; i < graduates.length; i++){
+  if (graduates[i].university.indexOf('Uni') > 0) {
+    unisWithUni.push(graduates[i].university);
+  }
+}
 console.log(unisWithUni);
 
 
